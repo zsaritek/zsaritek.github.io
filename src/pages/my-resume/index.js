34610fd07707e915048f48dbe1cd5aa9815bb597
zsaritek <zsaritek@gmail.com>
2024-01-06 17:35:35 +0100

    resume2

--- src/pages/my-resume/index.js
+++ src/pages/my-resume/index.js
@@ -45,9 +45,9 @@ export const MyResume = () => {
       <div>
         <h2>Ready to dive into my professional journey? Download my CV with a click, and let's connect on a whole new level.</h2>
         <div className="d-flex justify-content-center align-items-center" style={{ height: '50vh' }}>
-          <button onClick={handleClick} className="btn btn-light mb-2 mt-2 border border-dark ">Upload My Resume</button>
+          <button onClick={handleClick} className="btn btn-light mb-2 mt-2 border border-dark ">Download My Resume</button>
         </div>
-        <h2>Your curiosity is the key – unlock my story and explore the possibilities! </h2>
+        <h2 className="d-flex justify-content-center align-items-center">Your curiosity is the key – unlock my story and explore the possibilities! </h2>
       </div>
     </HelmetProvider>
   );
