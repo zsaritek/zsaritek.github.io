5b189a60b57d356700a9da04f5f46df393c19402
zsaritek <zsaritek@gmail.com>
2024-01-06 17:31:29 +0100

    resume-details

--- src/pages/my-resume/index.js
+++ src/pages/my-resume/index.js
@@ -1,11 +1,31 @@
 
 import "./style.css";
-import React, { useState } from "react";
 import { Helmet, HelmetProvider } from "react-helmet-async";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import { meta } from "../../content_option";
 
 export const MyResume = () => {
+  const handleSave = () => {
+    const filePath = '/files/cv.pdf'; // Replace with your actual file path
+    const url = `${process.env.PUBLIC_URL}${filePath}`;
+
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'cv.pdf'; // Replace with the desired file name
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+  };
+
+  const handleClick = () => {
+    const userConfirmed = window.confirm('Double-check my downloaded CV to ensure it is the hero you expected?');
+
+    if (userConfirmed) {
+      handleSave();
+    }
+  };
+
   return (
     <HelmetProvider>
       <Container className="About-header">
@@ -23,10 +43,11 @@ export const MyResume = () => {
       </Container>
 
       <div>
-        <h2>Download PDF</h2>
-        <a href={process.env.PUBLIC_URL + '/files/cv.pdf'} download='cv.pdf'>
-          Download PDF
-        </a>
+        <h2>Ready to dive into my professional journey? Download my CV with a click, and let's connect on a whole new level.</h2>
+        <div className="d-flex justify-content-center align-items-center" style={{ height: '50vh' }}>
+          <button onClick={handleClick} className="btn btn-light mb-2 mt-2 border border-dark ">Upload My Resume</button>
+        </div>
+        <h2>Your curiosity is the key – unlock my story and explore the possibilities! </h2>
       </div>
     </HelmetProvider>
   );
